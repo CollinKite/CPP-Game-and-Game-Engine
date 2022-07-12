f6e97ce531399d803533b85b01c302577148f5ce
Collin Kite <me@collinkite.com>
2022-07-12 09:26:26 -0600

    Updated Operators

--- Engine/Math/Vector2.h
+++ Engine/Math/Vector2.h
@@ -8,23 +8,41 @@ namespace crae
 		Vector2() : x{0}, y{0}{}
 		Vector2(float x, float y) : x{x}, y{y}{}
 		Vector2(float v) : x{v}, y{v}{}
+		Vector2(int x, int y) : x{ (float)x }, y{ (float)y }{}
 
 		void Set(float x, float y) { this->x = x; this->y = y; }
 		
+		//Vec2 = Vec2 + Vec2
+		Vector2 operator + (const Vector2& v) const { return Vector2{ this->x + v.x, this->y + v.y }; } //add 2 vectors together and return a new vector
+		Vector2 operator - (const Vector2& v) const { return Vector2{ this->x - v.x, this->y - v.y }; }
+		Vector2 operator * (const Vector2& v) const { return Vector2{ this->x * v.x, this->y * v.y }; }
+		Vector2 operator / (const Vector2& v) const { return Vector2{ this->x / v.x, this->y / v.y }; }
 
-		Vector2 operator + (const Vector2& v) { return Vector2{ this->x + v.x, this->y + v.y }; } //add 2 vectors together
-		Vector2 operator - (const Vector2& v) { return Vector2{ this->x - v.x, this->y - v.y }; } 
-		Vector2 operator * (const Vector2& v) { return Vector2{ this->x * v.x, this->y * v.y }; } 
-		Vector2 operator / (const Vector2& v) { return Vector2{ this->x / v.x, this->y / v.y }; } 
+		//Vector 2 = Vec2 + float
+		Vector2 operator + (float s) const { return Vector2{ this->x + s, this->y + s }; }
+		Vector2 operator - (float s) const { return Vector2{ this->x - s, this->y - s }; }
+		Vector2 operator * (float s) const { return Vector2{ this->x * s, this->y * s }; }
+		Vector2 operator / (float s) const { return Vector2{ this->x / s, this->y / s }; }
 
+		//Assignment Operators
 		Vector2 operator += (const Vector2& v) { this->x += v.x; this->y += v.y; return *this; }
 		Vector2 operator -= (const Vector2& v) { this->x -= v.x; this->y -= v.y; return *this; }
 		Vector2 operator *= (const Vector2& v) { this->x *= v.x; this->y *= v.y; return *this; }
 		Vector2 operator /= (const Vector2& v) { this->x /= v.x; this->y /= v.y; return *this; }
 
-		Vector2 operator + (float s) { return Vector2{ this->x + s, this->y + s }; }
-		Vector2 operator - (float s) { return Vector2{ this->x - s, this->y - s }; }
-		Vector2 operator * (float s) { return Vector2{ this->x * s, this->y * s }; }
-		Vector2 operator / (float s) { return Vector2{ this->x / s, this->y / s }; }
+		//Assignment Operators w/ float
+		Vector2 operator += (float s) { this->x += s; this->y += s; return *this; }
+		Vector2 operator -= (float s) { this->x -= s; this->y -= s; return *this; }
+		Vector2 operator *= (float s) { this->x *= s; this->y *= s; return *this; }
+		Vector2 operator /= (float s) { this->x /= s; this->y /= s; return *this; }
+
+		//unary
+		//Vector2 = -Vector2
+		Vector2 operator - () const { return Vector2{-x, -y}; }
+
+		//Comparison
+		//Vector2 == Vector2
+		bool operator == (const Vector2& v) const { return (this->x == v.x && this->y == v.y); }
+		bool operator != (const Vector2& v) const { return (this->x != v.x || this->y != v.y); }
 	};
 }
